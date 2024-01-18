8ad1e2498731f3ab295d0fcea97f895794349fab
jungwook <aking0520@naver.com>
2024-01-18 18:29:42 +0900

    혜택 페이지

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,10 @@ import './components/CounterComponent'
 import CounterComponent from './components/CounterComponent'
 import Introduce from './components/introduce'
 
+import App from './App.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Introduce/>
-    < CounterComponent/>
+    <App />
   </React.StrictMode>,
 )
